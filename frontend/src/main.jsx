083dd61d477b0ee3534b9e9ac10f422d4592c57f
Rohan Kumar Pandey <rohan@Rohans-MacBook-Air.local>
2024-08-19 07:42:12 +0530

    added frontend of the project

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query"
+import toast, { Toaster } from 'react-hot-toast';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+const queryClient = new QueryClient()
+
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<React.StrictMode>
+		<BrowserRouter>
+		<QueryClientProvider client={queryClient}>
+		<App />
+		</QueryClientProvider>
+			
+		<Toaster />
+		</BrowserRouter>
+	</React.StrictMode>
+);
